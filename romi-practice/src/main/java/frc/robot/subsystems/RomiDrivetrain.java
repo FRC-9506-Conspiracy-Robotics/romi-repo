5984cb3cf4a2bd4529da4596ffde2c3f1df42863
michael <tazedmold@gmail.com>
2025-11-06 18:27:21 -0600

    added comment

--- romi-practice/src/main/java/frc/robot/subsystems/RomiDrivetrain.java
+++ romi-practice/src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -13,6 +13,7 @@ public class RomiDrivetrain extends SubsystemBase {
   private static final double kCountsPerRevolution = 1440.0;
   private static final double kWheelDiameterInch = 2.75591; // 70 mm
 
+  // robot comment
   // The Romi has the left and right motors set to
   // PWM channels 0 and 1 respectively
   private final Spark m_leftMotor = new Spark(0);
